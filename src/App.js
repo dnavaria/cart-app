32b686bf02136f955a24f97af8e2b6b6e701ec9d
dnavaria <dk17816@gmail.com>
2022-09-03 19:26:43 +0530

    implemented cart qty increase and decrease functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Cart APP</h1>
-      </header>
+        <h1>Cart APP</h1>
+        </header>
+      <CartItem/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/CartItem.js
@@ -0,0 +1,59 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+        // this.increase_quantity = this.increase_quantity.bind(this);
+        // this.decrease_quantity = this.decrease_quantity.bind(this);
+    }
+    increase_quantity = ()=>  {
+        this.state.qty += 1;
+        console.log(this.state.qty);
+    }
+    decrease_quantity = ()=> {
+        if (this.state.qty == 0){
+            return;
+        }
+        this.state.qty -= 1;
+        console.log(this.state.qty);
+    }
+    render() {
+        const { price, title, qty, img } = this.state;
+        return (
+            <div className="cart-item">
+                <div className="left-block">
+                    <img src={img} alt="" style={style.image} />
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize: 25, color: '#555' }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: '#777' }}>Qty: {qty}</div>
+                    <div className="cart-item-actions">
+                        {/* Buttons */}
+                        <a className="action-icons" href="#0" onClick={this.increase_quantity}><i className="bi bi-patch-plus"></i></a>
+                        <a className="action-icons" href="#0" onClick={this.decrease_quantity}><i className="bi bi-patch-minus"></i></a>
+                        <a className="action-icons" href="#0" ><i className="bi bi-trash"></i></a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const style = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
+    }
+}
+export default CartItem;
